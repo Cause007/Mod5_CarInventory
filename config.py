c8798fcc15cda47249a78b0385aaf7cff6d07d0a
Evan Cosman <evancosman@gmail.com>
2024-02-25 09:47:07 -0800

    finished app

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config():
         Otherwise, create the config variable if not done already.
     '''
 
-FLASK_APP = os.getenv('FLASK_APP')
-FLASK_ENV = os.getenv('FLASK_ENV')
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'There is no @va, only Z0ol.'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_NOTIFICATIONS = False
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'There is no @va, only Z0ol.'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
